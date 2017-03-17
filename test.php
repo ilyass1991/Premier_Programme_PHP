f83bf00a943186a1c2752d33492f6a30f44c5e76
iliass.daoud <iliass.daoud@gmail.com>
2017-03-17 18:14:54 +0000

    l'affichage d'un message

--- test.php
+++ test.php
@@ -7,5 +7,6 @@
     <body>
         <h1>Ma page web 2</h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+        <?php echo '<strong>Bonjour à tous</strong>'; ?>
     </body>
 </html>

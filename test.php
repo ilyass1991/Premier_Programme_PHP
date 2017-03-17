57dd18d4cab1f22ab52940ec59f8622e2a1f2942
ilyass1991 <iliass.daoud@gmail.com>
2017-03-17 18:19:57 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>Ceci est une page de test avec des balises PHP</title>
         <meta charset="utf-8" />
-        <title>Ma page web</title>
     </head>
     <body>
-        <h1>Ma page web 2</h1>
-        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
-        <?php echo '<strong>Bonjour à tous</strong>'; ?>
+        <h2>Page de test</h2>
+        
+        <p>
+            Cette page contient du code HTML avec des balises PHP.<br />
+            <?php echo 'Cette phrase est générée par PHP.' ?>
+            Voici quelques petits tests :
+        </p>
+        
+        <ul>
+        <li style="color: blue;">Texte en bleu</li>
+        <li style="color: red;">Texte en rouge</li>
+        <li style="color: green;">Texte en vert</li>
+        </ul>
+        
         <?php
-
-     echo 'Ma première instruction.';
-     echo 'Ma deuxième instruction.';
-     echo 'Et en voilà une troisième !';
- 
- ?>
+           echo 'Cette phrase est aussi générée par PHP.'
+        ?>
     </body>
 </html>

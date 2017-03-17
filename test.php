99c72d782caf5ac1ed90e0d57988f61a6b1de263
ilyass1991 <iliass.daoud@gmail.com>
2017-03-17 18:17:10 +0000

    Update test.php

--- test.php
+++ test.php
@@ -8,5 +8,12 @@
         <h1>Ma page web 2</h1>
         <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
         <?php echo '<strong>Bonjour à tous</strong>'; ?>
+        <?php
+
+     echo 'Ma première instruction.';
+     echo 'Ma deuxième instruction.';
+     echo 'Et en voilà une troisième !';
+ 
+ ?>
     </body>
 </html>
